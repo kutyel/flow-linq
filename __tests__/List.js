63d16085478302f77674ff3e510450f4b0aa8773
kutyel <flaviocorpa@gmail.com>
2016-11-17 12:23:20 +0100

    Unit tests for add() and addRange()

--- __tests__/List.js
+++ __tests__/List.js
@@ -1,9 +1,20 @@
+// @flow
+
 import List from '../src/List';
 
 describe('List class', () => {
 
-    it('should create a List object without crashing', () => {
-        expect(new List()).toBeTruthy();
+    it('add', () => {
+        const string = 'string';
+        const list: List<string> = new List();
+        list.add(string);
+        expect(list.first()).toBe(string);
+    });
+
+    it('addRange', () => {
+        const list: List<number> = new List();
+        list.addRange([1, 2, 3, 4, 5]);
+        expect(list.toArray().toString()).toBe('1,2,3,4,5');
     });
 
 });
