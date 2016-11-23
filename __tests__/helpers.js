f781b374e583dc5eadef5f0d1169c1f4c7fa90f0
kutyel <flaviocorpa@gmail.com>
2016-11-23 11:49:05 +0100

    Unit tests for helpers

---
+++ __tests__/helpers.js
@@ -0,0 +1,15 @@
+// @flow
+
+import { compare } from '../src/helpers';
+
+describe('helper functions', () => {
+
+    test('compare', () => {
+        expect(compare(0, 1, x => x)).toBe(-1);
+    });
+
+    test('compare with descending parameter', () => {
+        expect(compare(0, 1, x => x, true)).toBe(1);
+    });
+
+});
