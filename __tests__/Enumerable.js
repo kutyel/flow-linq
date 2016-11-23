2db96cd660e0c569d33bbb95587aed75aaaead9e
kutyel <flaviocorpa@gmail.com>
2016-11-23 10:17:18 +0100

    Test Enumerable native arrays

--- __tests__/Enumerable.js
+++ __tests__/Enumerable.js
@@ -6,15 +6,16 @@ import Enumerable from '../src/Enumerable';
 describe('Enumerable class', () => {
 
     test('range', () => {
-        const expected = '1,2,3,4,5,6,7,8,9,10';
-        expect(Enumerable.range(1, 10).toArray().toString()).toEqual(expected);
+        const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        expect(Enumerable.range(1, 10).toArray()).toEqual(expected);
     });
 
     test('repeat', () => {
-        const test: List<string> = Enumerable.repeat('I like programming', 3);
-        expect(test.elementAt(0)).toBe('I like programming');
-        expect(test.elementAt(1)).toBe('I like programming');
-        expect(test.elementAt(2)).toBe('I like programming');
+        const repeated: string = 'I like programming';
+        const test: List<string> = Enumerable.repeat(repeated, 3);
+        expect(test.elementAt(0)).toBe(repeated);
+        expect(test.elementAt(1)).toBe(repeated);
+        expect(test.elementAt(2)).toBe(repeated);
         expect(test.elementAt(3)).toBeUndefined();
     });
 
